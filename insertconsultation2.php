149efa30a9e35128ccad5e38dbda4b08be1e8d6b
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-26 18:04:50 +0000

    Create insertconsultation2.php

---
+++ insertconsultation2.php
@@ -0,0 +1,52 @@
+<html>
+  <body>
+    <form action="checkclient.php" method="post">
+    <?php
+      $host = "db.tecnico.ulisboa.pt";
+      $user = "ist187094";
+      $pass = "stlk1710";
+      $dsn = "mysql:host=$host;dbname=$user";
+      try
+      {
+        $connection = new PDO($dsn, $user, $pass);
+      }
+      catch(PDOException $exception)
+      {
+        echo("<p>Error: ");
+        echo($exception->getMessage());
+        echo("</p>");
+        exit();
+      }
+
+      $VAT_doctor = $_REQUEST['VAT_doctor'];
+      $date_timestamp_aux = strtotime($_REQUEST['date_timestamp']);
+      $date_timestamp = date("Y-m-d H:i:s", $date_timestamp_aux);
+      $VAT_nurse = $_REQUEST['VAT_nurse'];
+
+      $sql = "INSERT INTO consultation_assistant VALUES ('$VAT_doctor', '$date_timestamp', '$VAT_nurse')";
+      echo("<p>$sql</p>");
+      $nrows = $connection->exec($sql);
+      if ($nrows == FALSE)
+      {
+        $info = $connection->errorInfo();
+        echo("<p>Error: {$info[2]}</p>");
+        exit();
+      }
+      echo("<p>Rows inserted in (consultation_assistant): $nrows</p>");
+
+
+      echo("<h3>Insert more information on the consultation:</h3>");
+
+      echo("<td><a href=\"insertnurse.php?VAT_doctor=".$VAT_doctor."&date_timestamp=".$date_timestamp);
+      echo("\">Add Nurse</a></p>\n");
+
+      echo("<p><a href=\"insertdiagnostic.php?VAT_doctor=".$VAT_doctor."&date_timestamp=".$date_timestamp);
+      echo("\">Add Diagnostic</a></p>\n");
+
+
+
+      $connection = null;
+    ?>
+    <p><input type="submit" value="Finish"/></p>
+  </body>
+</html>
