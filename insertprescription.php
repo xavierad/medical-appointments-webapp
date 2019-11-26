edd0d9a839b0f45ee10d521bc445bce979bf04f2
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-26 23:36:29 +0000

    Update insertprescription.php

--- insertprescription.php
+++ insertprescription.php
@@ -6,7 +6,23 @@
     $VAT_doctor = $_REQUEST['VAT_doctor'];
     $date_timestamp_aux = strtotime($_REQUEST['date_timestamp']);
     $date_timestamp = date("Y-m-d H:i:s", $date_timestamp_aux);
-    $dcID = $_REQUEST['dcID'];
+    $dcID = $_REQUEST['ID'];
+
+    $host = "db.ist.utl.pt";
+    $user = "ist187094";
+    $pass = "stlk1710";
+    $dsn = "mysql:host=$host;dbname=$user";
+    try
+    {
+      $connection = new PDO($dsn, $user, $pass);
+    }
+    catch(PDOException $exception)
+    {
+      echo("<p>Error: ");
+      echo($exception->getMessage());
+      echo("</p>");
+      exit();
+    }
 
     $sql = "INSERT INTO consultation_diagnostic VALUES ('$VAT_doctor', '$date_timestamp', '$dcID')";
     echo("<p>$sql</p>");
@@ -19,17 +35,20 @@
     }
     echo("<p>Rows inserted in (consultation_diagnostic): $nrows</p>");
     $connection = null;
+
+    echo("<h3>Insert a prescription for the diagnostic:</h3>");
     ?>
 
     <p><input type="hidden" name="VAT_doctor"
     value="<?=$VAT_doctor?>"/></p>
     <p><input type="hidden" name="date_timestamp"
     value="<?=$date_timestamp?>"/></p>
-
+    <p><input type="hidden" name="dcID"
+    value="<?=$dcID?>"/></p>
 
     <p>Name:
       <select name="_name">
-  <?php
+    <?php
     $host = "db.ist.utl.pt";
     $user = "ist187094";
     $pass = "stlk1710";
@@ -57,17 +76,37 @@
 
     foreach($result as $row)
     {
-      $dcID = $row['ID'];
-      echo("<option value=\"$Pname\">$Pname</option>");
+      $_name = $row['_name'];
+      echo("<option value=\"$_name\">$_name</option>");
     }
+
 
     $connection = null;
   ?>
     </select>
     </p>
 
+    <p>Dosage: <input type="text" name="dosage" required/></p>
+    <p>Description: <input type="text" name="_description" required/></p>
+
+
+
 
   <p><input type="submit" value="Add"/></p>
+  </form>
+
+  <form action="insertconsultation5.php" method="post">
+  <input type="submit" value="No prescription">
+  <?php
+  $VAT_doctor = $_REQUEST['VAT_doctor'];
+  $date_timestamp_aux = strtotime($_REQUEST['date_timestamp']);
+  $date_timestamp = date("Y-m-d H:i:s", $date_timestamp_aux);
+  ?>
+  <p><input type="hidden" name="VAT_doctor"
+  value="<?=$VAT_doctor?>"/></p>
+  <p><input type="hidden" name="date_timestamp"
+  value="<?=$date_timestamp?>"/></p>
+  </form>
 
   </body>
 </html>
