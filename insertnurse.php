103a71af4c73c7946ff5b3da69d563ded430ddf7
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-26 18:04:56 +0000

    Create insertnurse.php

---
+++ insertnurse.php
@@ -0,0 +1,63 @@
+<html>
+  <body>
+  <form action="insertconsultation2.php" method="post">
+    <?php
+
+      $VAT_doctor = $_REQUEST['VAT_doctor'];
+      $date_timestamp_aux = strtotime($_REQUEST['date_timestamp']);
+      $date_timestamp = date("Y-m-d H:i:s", $date_timestamp_aux);
+
+      echo("<h3>Insert a nurse for the consultation:</h3>");
+
+
+    ?>
+
+    <p><input type="hidden" name="VAT_doctor"
+    value="<?=$VAT_doctor?>"/></p>
+    <p><input type="hidden" name="date_timestamp"
+    value="<?=$date_timestamp?>"/></p>
+
+    <p>Nurse (VAT):
+      <select name="VAT_nurse">
+  <?php
+    $host = "db.ist.utl.pt";
+    $user = "ist187094";
+    $pass = "stlk1710";
+    $dsn = "mysql:host=$host;dbname=$user";
+    try
+    {
+      $connection = new PDO($dsn, $user, $pass);
+    }
+    catch(PDOException $exception)
+    {
+      echo("<p>Error: ");
+      echo($exception->getMessage());
+      echo("</p>");
+      exit();
+    }
+
+    $sql = "SELECT VAT FROM nurse ORDER BY VAT";
+    $result = $connection->query($sql);
+    if ($result == FALSE)
+    {
+      $info = $connection->errorInfo();
+      echo("<p>Error: {$info[2]}</p>");
+      exit();
+    }
+
+    foreach($result as $row)
+    {
+      $VAT_nurse = $row['VAT'];
+      echo("<option value=\"$VAT_nurse\">$VAT_nurse</option>");
+    }
+
+    $connection = null;
+  ?>
+    </select>
+    </p>
+
+
+  <p><input type="submit" value="Add"/></p>
+
+  </body>
+</html>
