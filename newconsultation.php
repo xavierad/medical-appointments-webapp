1842dcbd47690ffc81089d6c015873378f776da0
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-26 23:36:33 +0000

    Update newconsultation.php

--- newconsultation.php
+++ newconsultation.php
@@ -1,6 +1,6 @@
 <html>
   <body>
-  <form action="insertconsultation.php" method="post">
+  <form action="insertconsultation1.php" method="post">
 <?php
     if ($tok !== false) {
       $tok = strtok($_REQUEST['appointment'], ",");
@@ -27,58 +27,12 @@
   <p><input type="hidden" name="date_timestamp"
   value="<?=$date_timestamp?>"/></p>
 
-  <p>Nurse (VAT):
-    <select name="VAT_nurse">
-<?php
-  $host = "db.ist.utl.pt";
-  $user = "ist187094";
-  $pass = "stlk1710";
-  $dsn = "mysql:host=$host;dbname=$user";
-  try
-  {
-    $connection = new PDO($dsn, $user, $pass);
-  }
-  catch(PDOException $exception)
-  {
-    echo("<p>Error: ");
-    echo($exception->getMessage());
-    echo("</p>");
-    exit();
-  }
+  <p>SOAP S: <input type="text" name="SOAP_S" required/></p>
+  <p>SOAP O: <input type="text" name="SOAP_O" required/></p>
+  <p>SOAP A: <input type="text" name="SOAP_A" required/></p>
+  <p>SOAP P: <input type="text" name="SOAP_P" required/></p>
 
-  $sql = "SELECT VAT FROM nurse ORDER BY VAT";
-  $result = $connection->query($sql);
-  if ($result == FALSE)
-  {
-    $info = $connection->errorInfo();
-    echo("<p>Error: {$info[2]}</p>");
-    exit();
-  }
-
-  foreach($result as $row)
-  {
-    $VAT_nurse = $row['VAT'];
-    echo("<option value=\"$VAT_nurse\">$VAT_nurse</option>");
-  }
-
-  $connection = null;
-?>
-  </select>
-  </p>
-
-
-  <p>SOAP S: <input type="text" name="SOAP_S"/></p>
-  <p>SOAP O: <input type="text" name="SOAP_O"/></p>
-  <p>SOAP A: <input type="text" name="SOAP_A"/></p>
-  <p>SOAP P: <input type="text" name="SOAP_P"/></p>
-  <p>Diagnostic code ID: <input type="text" name="dcID"/></p>
-  <p>Diagnostic code description: <input type="text" name="dcDescription"/></p>
-  <p>Prescription name: <input type="text" name="pName"/></p>
-  <p>Prescription lab: <input type="text" name="pLab"/></p>
-  <p>Prescription dosage: <input type="text" name="pDosage"/></p>
-  <p>Prescription description: <input type="text" name="pDescription"/></p>
-
-  <p><input type="submit" value="Submit"/></p>
-
+  <p><input type="submit" value="Continue"/></p>
+  </form>
   </body>
 </html>
