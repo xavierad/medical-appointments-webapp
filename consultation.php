3be348a983630a85b308d5f5d323b841402ce043
pedrogcmartin <48490908+pedrogcmartin@users.noreply.github.com>
2019-11-29 17:53:09 +0000

    Update consultation.php

--- consultation.php
+++ consultation.php
@@ -22,6 +22,40 @@
     try
     {
       $connection = new PDO($dsn, $user, $pass);
+
+
+
+      $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P
+              FROM consultation C
+              WHERE (C.VAT_doctor NOT IN (SELECT CD.VAT_doctor FROM consultation_diagnostic CD)
+                OR C.date_timestamp NOT IN (SELECT CD.date_timestamp FROM consultation_diagnostic CD))
+              AND C.VAT_doctor = :VAT_doctor
+              AND C.date_timestamp = :date_timestamp";
+
+      $stmt = $connection->prepare($sql);
+      if ($stmt == FALSE)
+      {
+        $info = $connection->errorInfo();
+        echo("<p>Error: {$info[2]}</p>");
+        exit();
+      }
+      else {
+        $stmt->bindParam(':VAT_doctor', $VAT_doctor);
+        $stmt->bindParam(':date_timestamp', $date_timestamp);
+        $stmt->execute();
+
+        foreach($stmt as $row)
+        {
+          echo("<tr>\n");
+          echo("<td>{$row['VAT_doctor']}</td>\n");
+          echo("<td>{$row['date_timestamp']}</td>\n");
+          echo("<td>{$row['SOAP_S']}</td>\n");
+          echo("<td>{$row['SOAP_O']}</td>\n");
+          echo("<td>{$row['SOAP_A']}</td>\n");
+          echo("<td>{$row['SOAP_P']}</td>\n");
+          echo("</tr>\n");
+        }
+      }
     }
     catch(PDOException $exception)
     {
@@ -30,35 +64,6 @@
       echo("</p>");
       exit();
     }
-
-    $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P
-            FROM consultation C
-            WHERE (C.VAT_doctor NOT IN (SELECT CD.VAT_doctor FROM consultation_diagnostic CD)
-              OR C.date_timestamp NOT IN (SELECT CD.date_timestamp FROM consultation_diagnostic CD))
-            AND C.VAT_doctor = '$VAT_doctor'
-            AND C.date_timestamp = '$date_timestamp'";
-
-    $result = $connection->query($sql);
-    if ($result == FALSE)
-    {
-      $info = $connection->errorInfo();
-      echo("<p>Error: {$info[2]}</p>");
-      exit();
-    }
-    //echo("<table border=\"1\">");
-    //echo("<tr><td>VAT_doctor</td><td>date_timestamp</td><td>SOAP_S</td><td>SOAP_O</td><td>SOAP_A</td><td>SOAP_P</td><td>Diagnostic ID</td><td>Diagnostic Description</td><td>Prescription Name</td><td>Prescription Lab</td><td>Prescription Dosage</td><td>Prescription Description</td></tr>");
-    foreach($result as $row)
-    {
-      echo("<tr>\n");
-      echo("<td>{$row['VAT_doctor']}</td>\n");
-      echo("<td>{$row['date_timestamp']}</td>\n");
-      echo("<td>{$row['SOAP_S']}</td>\n");
-      echo("<td>{$row['SOAP_O']}</td>\n");
-      echo("<td>{$row['SOAP_A']}</td>\n");
-      echo("<td>{$row['SOAP_P']}</td>\n");
-      echo("</tr>\n");
-    }
-    //echo("</table>\n\n");
     $connection = null;
 ?>
 
@@ -70,6 +75,44 @@
     try
     {
       $connection = new PDO($dsn, $user, $pass);
+
+
+      $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P, CD.ID, DC._description
+              FROM consultation C, consultation_diagnostic CD, diagnostic_code DC
+              WHERE CD.ID  NOT IN (SELECT P.ID FROM prescription P WHERE P.VAT_doctor=:VAT_doctor AND P.date_timestamp=:date_timestamp)
+              AND C.VAT_doctor = :VAT_doctor
+              AND C.date_timestamp = :date_timestamp
+              AND C.VAT_doctor = CD.VAT_doctor
+              AND C.date_timestamp = CD.date_timestamp
+              AND CD.ID = DC.ID";
+
+      $stmt = $connection->prepare($sql);
+      if ($stmt == FALSE)
+      {
+        $info = $connection->errorInfo();
+        echo("<p>Error: {$info[2]}</p>");
+        exit();
+      }
+
+      else {
+        $stmt->bindParam(':VAT_doctor', $VAT_doctor);
+        $stmt->bindParam(':date_timestamp', $date_timestamp);
+        $stmt->execute();
+
+        foreach($stmt as $row)
+        {
+          echo("<tr>\n");
+          echo("<td>{$row['VAT_doctor']}</td>\n");
+          echo("<td>{$row['date_timestamp']}</td>\n");
+          echo("<td>{$row['SOAP_S']}</td>\n");
+          echo("<td>{$row['SOAP_O']}</td>\n");
+          echo("<td>{$row['SOAP_A']}</td>\n");
+          echo("<td>{$row['SOAP_P']}</td>\n");
+          echo("<td>{$row['ID']}</td>\n");
+          echo("<td>{$row[7]}</td>\n");
+          echo("</tr>\n");
+        }
+      }
     }
     catch(PDOException $exception)
     {
@@ -78,40 +121,6 @@
       echo("</p>");
       exit();
     }
-
-    $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P, CD.ID, DC._description
-            FROM consultation C, consultation_diagnostic CD, diagnostic_code DC
-            WHERE CD.ID  NOT IN (SELECT P.ID FROM prescription P WHERE P.VAT_doctor='$VAT_doctor' AND P.date_timestamp='$date_timestamp')
-            AND C.VAT_doctor = '$VAT_doctor'
-            AND C.date_timestamp = '$date_timestamp'
-            AND C.VAT_doctor = CD.VAT_doctor
-            AND C.date_timestamp = CD.date_timestamp
-            AND CD.ID = DC.ID";
-
-    $result = $connection->query($sql);
-    if ($result == FALSE)
-    {
-      $info = $connection->errorInfo();
-      echo("<p>Error: {$info[2]}</p>");
-      exit();
-    }
-
-    //echo("<table border=\"1\">");
-    //echo("<tr><td>VAT_doctor</td><td>date_timestamp</td><td>SOAP_S</td><td>SOAP_O</td><td>SOAP_A</td><td>SOAP_P</td><td>Diagnostic ID</td><td>Diagnostic Description</td><td>Prescription Name</td><td>Prescription Lab</td><td>Prescription Dosage</td><td>Prescription Description</td></tr>");
-    foreach($result as $row)
-    {
-      echo("<tr>\n");
-      echo("<td>{$row['VAT_doctor']}</td>\n");
-      echo("<td>{$row['date_timestamp']}</td>\n");
-      echo("<td>{$row['SOAP_S']}</td>\n");
-      echo("<td>{$row['SOAP_O']}</td>\n");
-      echo("<td>{$row['SOAP_A']}</td>\n");
-      echo("<td>{$row['SOAP_P']}</td>\n");
-      echo("<td>{$row['ID']}</td>\n");
-      echo("<td>{$row[7]}</td>\n");
-      echo("</tr>\n");
-    }
-    //echo("</table>\n\n");
     $connection = null;
 ?>
 
@@ -123,6 +132,50 @@
     try
     {
       $connection = new PDO($dsn, $user, $pass);
+
+
+      $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P, DC.ID, DC._description, P._name, P.lab, P.dosage, P._description
+              FROM consultation C, consultation_diagnostic CD, diagnostic_code DC, prescription P
+              WHERE C.VAT_doctor = :VAT_doctor
+              AND C.date_timestamp = :date_timestamp
+              AND C.VAT_doctor = CD.VAT_doctor
+              AND C.date_timestamp = CD.date_timestamp
+              AND CD.ID = DC.ID
+              AND CD.VAT_doctor = P.VAT_doctor
+              AND CD.date_timestamp = P.date_timestamp
+              AND CD.ID = P.ID";
+
+      $stmt = $connection->prepare($sql);
+      if ($stmt == FALSE)
+      {
+        $info = $connection->errorInfo();
+        echo("<p>Error: {$info[2]}</p>");
+        exit();
+      }
+
+      else {
+        $stmt->bindParam(':VAT_doctor', $VAT_doctor);
+        $stmt->bindParam(':date_timestamp', $date_timestamp);
+        $stmt->execute();
+
+        foreach($stmt as $row)
+        {
+          echo("<tr>\n");
+          echo("<td>{$row['VAT_doctor']}</td>\n");
+          echo("<td>{$row['date_timestamp']}</td>\n");
+          echo("<td>{$row['SOAP_S']}</td>\n");
+          echo("<td>{$row['SOAP_O']}</td>\n");
+          echo("<td>{$row['SOAP_A']}</td>\n");
+          echo("<td>{$row['SOAP_P']}</td>\n");
+          echo("<td>{$row['ID']}</td>\n");
+          echo("<td>{$row[7]}</td>\n");
+          echo("<td>{$row['_name']}</td>\n");
+          echo("<td>{$row['lab']}</td>\n");
+          echo("<td>{$row['dosage']}</td>\n");
+          echo("<td>{$row['_description']}</td>\n");
+          echo("</tr>\n");
+        }
+      }
     }
     catch(PDOException $exception)
     {
@@ -132,47 +185,10 @@
       exit();
     }
 
-    $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P, DC.ID, DC._description, P._name, P.lab, P.dosage, P._description
-            FROM consultation C, consultation_diagnostic CD, diagnostic_code DC, prescription P
-            WHERE C.VAT_doctor = '$VAT_doctor'
-            AND C.date_timestamp = '$date_timestamp'
-            AND C.VAT_doctor = CD.VAT_doctor
-            AND C.date_timestamp = CD.date_timestamp
-            AND CD.ID = DC.ID
-            AND CD.VAT_doctor = P.VAT_doctor
-            AND CD.date_timestamp = P.date_timestamp
-            AND CD.ID = P.ID";
-
-    $result = $connection->query($sql);
-    if ($result == FALSE)
-    {
-      $info = $connection->errorInfo();
-      echo("<p>Error: {$info[2]}</p>");
-      exit();
-    }
-
-    //echo("<table border=\"1\">");
-    //echo("<tr><td>VAT_doctor</td><td>date_timestamp</td><td>SOAP_S</td><td>SOAP_O</td><td>SOAP_A</td><td>SOAP_P</td><td>Diagnostic ID</td><td>Diagnostic Description</td><td>Prescription Name</td><td>Prescription Lab</td><td>Prescription Dosage</td><td>Prescription Description</td></tr>");
-    foreach($result as $row)
-    {
-      echo("<tr>\n");
-      echo("<td>{$row['VAT_doctor']}</td>\n");
-      echo("<td>{$row['date_timestamp']}</td>\n");
-      echo("<td>{$row['SOAP_S']}</td>\n");
-      echo("<td>{$row['SOAP_O']}</td>\n");
-      echo("<td>{$row['SOAP_A']}</td>\n");
-      echo("<td>{$row['SOAP_P']}</td>\n");
-      echo("<td>{$row['ID']}</td>\n");
-      echo("<td>{$row[7]}</td>\n");
-      echo("<td>{$row['_name']}</td>\n");
-      echo("<td>{$row['lab']}</td>\n");
-      echo("<td>{$row['dosage']}</td>\n");
-      echo("<td>{$row['_description']}</td>\n");
-      echo("</tr>\n");
-    }
     echo("</table>\n\n");
     echo("<td><a href=\"newprocedure.php?VAT_doctor=".$row['VAT_doctor']."&date_timestamp=".$row['date_timestamp']);
     echo("\"><p>Insert Dental Charting Procedure</p></a></td>\n");
+
     $connection = null;
 ?>
 
