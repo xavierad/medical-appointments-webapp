1771dce64eaa1a7f54a0c2625e9d1f2313212385
joaoaptavares <57359578+joaoaptavares@users.noreply.github.com>
2019-12-01 23:31:28 +0000

    prepared statements

--- insertcharting.php
+++ insertcharting.php
@@ -8,6 +8,106 @@
       try
       {
         $connection = new PDO($dsn, $user, $pass);
+
+        $_name = "Dental charting";
+        $VAT_doctor = $_REQUEST['VAT_doctor'];
+        $date_timestamp_aux = strtotime($_REQUEST['date_timestamp']);
+        $date_timestamp = date("Y-m-d H:i:s", $date_timestamp_aux);
+        $_description = $_REQUEST['_description'];
+        $insertions = $_REQUEST['insertions'];
+
+        $connection->beginTransaction();
+
+        $sql = "INSERT IGNORE INTO procedure_in_consultation VALUES (:_name, :VAT_doctor, :date_timestamp, :_description)";
+        echo("<p>$sql</p>");
+        $stmt = $connection->prepare($sql);
+
+        if ($stmt== FALSE)
+        {
+          $info = $connection->errorInfo();
+          echo("<p>Error: {$info[2]}</p>");
+          #exit();
+        }
+        else{
+          $stmt->bindParam(':_name', $_name);
+          $stmt->bindParam(':VAT_doctor', $VAT_doctor);
+          $stmt->bindParam(':date_timestamp', $date_timestamp);
+          $stmt->bindParam(':_description', $_description);
+          # execution
+          $stmt->execute();
+          $nrows = $stmt->rowCount();
+          echo("<strong>A new client was added: $nrows !</strong></div></div>");
+          if ($nrows==1){
+            echo("<br><div class=\"container\">");
+            echo("<div class=\"alert alert-success\">");
+            echo("<strong>A new client was added!</strong></div></div>");
+            #echo("<p>A new client, $name ($VAT) was added!</p>");
+          }
+        }
+
+        $sql = "DELETE IGNORE FROM procedure_charting WHERE VAT = :VAT_doctor AND date_timestamp = :date_timestamp";
+        echo("<p>$sql</p>");
+        $stmt = $connection->prepare($sql);
+
+        if ($stmt== FALSE)
+        {
+          $info = $connection->errorInfo();
+          echo("<p>Error: {$info[2]}</p>");
+          #exit();
+        }
+        else{
+          $stmt->bindParam(':VAT_doctor', $VAT_doctor);
+          $stmt->bindParam(':date_timestamp', $date_timestamp);
+          # execution
+          $stmt->execute();
+          $nrows = $stmt->rowCount();
+          echo("<strong>A new client was added: $nrows !</strong></div></div>");
+          if ($nrows==1){
+            echo("<br><div class=\"container\">");
+            echo("<div class=\"alert alert-success\">");
+            echo("<strong>A new client was added!</strong></div></div>");
+            #echo("<p>A new client, $name ($VAT) was added!</p>");
+          }
+        }
+
+        $sql = "INSERT INTO procedure_charting VALUES (:_name, :VAT_doctor, :date_timestamp, :quadrant, :_number, :_desc, :measure)";
+        echo("<p>$sql</p>");
+        $stmt = $connection->prepare($sql);
+
+        if ($stmt== FALSE)
+        {
+          $info = $connection->errorInfo();
+          echo("<p>Error: {$info[2]}</p>");
+          $connection->rollback();
+          #exit();
+        }
+        else{
+          for($i=0; $i < $_REQUEST['insertions']; $i++){
+
+            $quadrant = $_REQUEST["quadrant".$i];
+            $_number = $_REQUEST["_number".$i];
+            $measure = $_REQUEST["measure".$i];
+            $_desc = $_REQUEST["_desc".$i];
+
+          $stmt->bindParam(':_name', $_name);
+          $stmt->bindParam(':VAT_doctor', $VAT_doctor);
+          $stmt->bindParam(':date_timestamp', $date_timestamp);
+          $stmt->bindParam(':quadrant', $quadrant);
+          $stmt->bindParam(':_number', $_number);
+          $stmt->bindParam(':measure', $measure);
+          $stmt->bindParam(':_desc', $_desc);
+          # execution
+          $stmt->execute();
+          $nrows = $stmt->rowCount();
+          echo("<strong>A new client was added: $nrows !</strong></div></div>");
+          if ($nrows==1){
+            echo("<br><div class=\"container\">");
+            echo("<div class=\"alert alert-success\">");
+            echo("<strong>A new client was added!</strong></div></div>");
+            #echo("<p>A new client, $name ($VAT) was added!</p>");
+          }
+          }
+        }
       }
       catch(PDOException $exception)
       {
@@ -16,55 +116,6 @@
         echo("</p>");
         exit();
       }
-      $_name = "Dental charting";
-      $VAT_doctor = $_REQUEST['VAT_doctor'];
-      $date_timestamp_aux = strtotime($_REQUEST['date_timestamp']);
-      $date_timestamp = date("Y-m-d H:i:s", $date_timestamp_aux);
-      $_description = $_REQUEST['_description'];
-      $insertions = $_REQUEST['insertions'];
-
-      $connection->beginTransaction();
-
-      $sql = "INSERT IGNORE INTO procedure_in_consultation VALUES ('$_name', '$VAT_doctor', '$date_timestamp', '$_description')";
-      echo("<p>$sql</p>");
-      $nrows = $connection->exec($sql);
-
-      echo("<p>Rows inserted: $nrows</p>");
-
-      $sql = "DELETE IGNORE FROM procedure_charting WHERE VAT = '$VAT_doctor' AND date_timestamp = '$date_timestamp'";
-      echo("<p>$sql</p>");
-      $nrows = $connection->exec($sql);
-      if ($nrows == FALSE)
-      {
-        $info = $connection->errorInfo();
-        echo("<p>Error: {$info[2]}</p>");
-        #exit();
-      }
-      echo("<p>Rows deleted: $nrows</p>");
-
-      for($i=0; $i < $_REQUEST['insertions']; $i++){
-
-        $quadrant = $_REQUEST["quadrant".$i];
-        $_number = $_REQUEST["_number".$i];
-        $measure = $_REQUEST["measure".$i];
-        $_desc = $_REQUEST["_desc".$i];
-
-        $sql = "INSERT INTO procedure_charting VALUES ('$_name', '$VAT_doctor', '$date_timestamp', '$quadrant', '$_number', '$_desc', '$measure')";
-        echo("<p>$sql</p>");
-        $nrows = $connection->exec($sql);
-        if ($nrows == FALSE)
-        {
-          $info = $connection->errorInfo();
-          $connection->rollback();
-          echo("<p>Error: {$info[2]}</p>");
-          exit();
-        }
-        echo("<p>Rows inserted: $nrows</p>");
-      }
-
-      echo("<td><a href=\"consultation.php?consultation=");
-      echo($VAT_doctor. "," .$date_timestamp);
-      echo("\">Back to Consultation Info</a></td>\n");
 
       $connection->commit();
       $connection = null;
