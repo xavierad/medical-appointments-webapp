02fd4d4f774642fe0363bf9b47ee37baebe2282c
joaoaptavares <57359578+joaoaptavares@users.noreply.github.com>
2019-12-02 14:29:15 +0000

    update JT

--- newprocedure.php
+++ newprocedure.php
@@ -13,11 +13,14 @@
           <input type="hidden" name="VAT_doctor" value="<?=$_REQUEST['VAT_doctor']?>"><p>
           <input type="hidden" name="date_timestamp" value="<?=$_REQUEST['date_timestamp']?>"></p>
         </div>
+       </div>
+        <div class="form-row">
         <div class="form-group col-md-4">
         <p>Description: <input type=text name="_description"/></p>
         </div>
         <div class="form-group col-md-4">
         <p>Number of insertions: <input type=number name="insertions" min="1" max="32" required/></p>
+        </div>
         </div>
         <p><input type="submit" class="btn btn-primary" value="Start"/></p>
     </form>
