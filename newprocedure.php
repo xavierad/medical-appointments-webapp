7ab1cf4df81c2f0e34a745e39de20f84481ab8f6
joaoaptavares <57359578+joaoaptavares@users.noreply.github.com>
2019-12-02 14:36:00 +0000

    JT

--- newprocedure.php
+++ newprocedure.php
@@ -4,27 +4,24 @@
   </head>
   <body>
    <div class="container">
-    <form action="newcharting.php" method="post">
-      <br>
+     <br>
+     <form action="newcharting.php" method="post">
       <h3>Insert a new procedure</h3>
-      <div class="form-row">
-        <div class="form-group col-md-4">
-        <p>Doctor: <?=$_REQUEST['VAT_doctor'].', '.$_REQUEST['date_timestamp']?>
+      <p>Doctor: <?=$_REQUEST['VAT_doctor'].', '.$_REQUEST['date_timestamp']?>
           <input type="hidden" name="VAT_doctor" value="<?=$_REQUEST['VAT_doctor']?>"><p>
           <input type="hidden" name="date_timestamp" value="<?=$_REQUEST['date_timestamp']?>"></p>
-        </div>
-       </div>
-        <div class="form-row">
+      <div class="form-row">
         <div class="form-group col-md-6">
-        <p>Description: <input type=text name="_description"/></p>
+          <p>Description: <input type=text name="_description"/></p>
         </div>
-        </div>
-        <div class="form-row">
+      </div>
+      <div class="form-row">
         <div class="form-group col-md-2">
-        <p>Number of insertions: <input type=number name="insertions" min="1" max="32" required/></p>
+          <p>Number of insertions: <input type=number name="insertions" min="1" max="32" required/></p>
         </div>
-        </div>
-        <p><input type="submit" class="btn btn-primary" value="Start"/></p>
+      </div>
+      <p><input type="submit" class="btn btn-primary" value="Start"/></p>
     </form>
+   </div>
   </body>
 </html>
