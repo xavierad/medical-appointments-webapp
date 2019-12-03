7c93afbda44b4b02e7786c51003f5639487099ea
xavierad <xdias_28@hotmail.com>
2019-12-03 11:44:05 +0000

    When there is no consultation or appointment

--- consultations_appointments.php
+++ consultations_appointments.php
@@ -29,24 +29,28 @@
       else {
         $stmt->bindParam(':VAT', $VAT);
         $stmt->execute();
-
-
+        $nrows = $stmt->rowCount();
         echo("<h3>Appointments:</h3>");
-        echo("<table border=\"1\" cellspacing=\"5\">\n");
-        echo("<tr><td><center>VAT doctor</center</td><td><center>Date</center</td><td><center>Description</center</td><td><center>VAT client</center</td></tr>");
-        foreach($stmt as $row)
-        {
-          echo("<tr>\n");
-          echo("<td><center>{$row['VAT_doctor']}</center</td>\n");
-          echo("<td><center>{$row['date_timestamp']}</center</td>\n");
-          echo("<td><center>{$row['_description']}</center</td>\n");
-          echo("<td><center>{$row['VAT_client']}</center</td>\n");
-          echo("<td><a href=\"appointment.php?appointment=");
-          echo($row['VAT_client']. "," .$row['date_timestamp']);
-          echo("\"><center>Appointment Info</center</a></td>\n");
-          echo("</tr>\n");
+        if($nrows==0){
+          echo("<p><strong>There is no appointments for this client</strong></p>");
         }
-        echo("</table>\n\n");
+        else{
+          echo("<table border=\"1\" cellspacing=\"5\">\n");
+          echo("<tr><td><center>VAT doctor</center</td><td><center>Date</center</td><td><center>Description</center</td><td><center>VAT client</center</td></tr>");
+          foreach($stmt as $row)
+          {
+            echo("<tr>\n");
+            echo("<td><center>{$row['VAT_doctor']}</center</td>\n");
+            echo("<td><center>{$row['date_timestamp']}</center</td>\n");
+            echo("<td><center>{$row['_description']}</center</td>\n");
+            echo("<td><center>{$row['VAT_client']}</center</td>\n");
+            echo("<td><a href=\"appointment.php?appointment=");
+            echo($row['VAT_client']. "," .$row['date_timestamp']);
+            echo("\"><center>Appointment Info</center</a></td>\n");
+            echo("</tr>\n");
+          }
+          echo("</table>\n\n");
+        }
       }
 
       $sql = "SELECT C.VAT_doctor, C.date_timestamp, C.SOAP_S, C.SOAP_O, C.SOAP_A, C.SOAP_P FROM consultation as C, appointment as A WHERE C.VAT_doctor = A.VAT_doctor AND C.date_timestamp = A.date_timestamp AND A.VAT_client = :VAT ORDER BY C.date_timestamp";
@@ -61,26 +65,31 @@
       else {
         $stmt->bindParam(':VAT', $VAT);
         $stmt->execute();
-
+        $nrows = $stmt->rowCount();
         echo("<br><br>");
         echo("<h3>Consultations:</h3>");
-        echo("<table border=\"1\" cellspacing=\"5\">\n");
-        echo("<tr><td><center>VAT doctor</center></td><td><center>Date</center></td><td><center>SOAP S</center></td><td><center>SOAP O</center></td><td><center>SOAP A</center></td><td><center>SOAP P</center></td></tr>");
-        foreach($stmt as $row)
-        {
-          echo("<tr>\n");
-          echo("<td><center>{$row['VAT_doctor']}</center></td>\n");
-          echo("<td><center>{$row['date_timestamp']}</center></td>\n");
-          echo("<td><center>{$row['SOAP_S']}</center></td>\n");
-          echo("<td><center>{$row['SOAP_O']}</center></td>\n");
-          echo("<td><center>{$row['SOAP_A']}</center></td>\n");
-          echo("<td><center>{$row['SOAP_P']}</center></td>\n");
-          echo("<td><a href=\"consultation.php?consultation=");
-          echo($row['VAT_doctor']. "," .$row['date_timestamp']);
-          echo("\"><center>Consultation Info</center></a></td>\n");
-          echo("</tr>\n");
+        if($nrows==0){
+          echo("<p><strong>There is no consultations for this client</strong></p>");
         }
-        echo("</table>\n");
+        else{
+          echo("<table cellpadding=\"5\" border=\"1\" cellspacing=\"2\">\n");
+          echo("<tr><td><center>VAT doctor</center></td><td><center>Date</center></td><td><center>SOAP S</center></td><td><center>SOAP O</center></td><td><center>SOAP A</center></td><td><center>SOAP P</center></td></tr>");
+          foreach($stmt as $row)
+          {
+            echo("<tr>\n");
+            echo("<td><center>{$row['VAT_doctor']}</center></td>\n");
+            echo("<td><center>{$row['date_timestamp']}</center></td>\n");
+            echo("<td><center>{$row['SOAP_S']}</center></td>\n");
+            echo("<td><center>{$row['SOAP_O']}</center></td>\n");
+            echo("<td><center>{$row['SOAP_A']}</center></td>\n");
+            echo("<td><center>{$row['SOAP_P']}</center></td>\n");
+            echo("<td><a href=\"consultation.php?consultation=");
+            echo($row['VAT_doctor']. "," .$row['date_timestamp']);
+            echo("\"><center>Consultation Info</center></a></td>\n");
+            echo("</tr>\n");
+          }
+          echo("</table>\n");
+        }
       }
     }
 
